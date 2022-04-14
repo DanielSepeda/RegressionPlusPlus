0d05d6de9c309bc60a34ef1b665dfe2fac555632
Daniel Sepeda <tech@gussag.com>
2022-04-13 21:40:49 -0700

    getting familiar with Matrix class

--- RegressionAnalysis/Linear.cpp
+++ RegressionAnalysis/Linear.cpp
@@ -1,2 +1,12 @@
 #include "Linear.hpp"
 
+
+static float GetSumXX(const Eigen::VectorXf& x, const Eigen::VectorXf& y)
+{
+	if (x.size() == 0 or y.size() == 0)
+	{
+		throw std::exception("invalid input");
+	}
+	const float x_sum{ x.size() - 1 / x.sum() };
+	const Eigen::VectorXf x_mean{};
+}
--- RegressionAnalysis/Linear.hpp
+++ RegressionAnalysis/Linear.hpp
@@ -2,6 +2,12 @@
 #define LINEAR_HPP
 #include <Eigen/Dense>
 
+class Core
+{
+public:
+	template <typename T>
+	static T GetSumXX(const Eigen::VectorXf& x, const Eigen::VectorXf& y);
+};
 
 #endif // !1
 
