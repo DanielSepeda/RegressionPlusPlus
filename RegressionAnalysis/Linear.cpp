f9ec34377698d9204c3c660ea91d9e8fbff65450
Daniel Sepeda <tech@gussag.com>
2022-04-14 21:22:59 -0700

    built a simple linear model from Eigen VectorXf.

--- RegressionAnalysis/Linear.cpp
+++ RegressionAnalysis/Linear.cpp
@@ -1,12 +1,113 @@
 #include "Linear.hpp"
 
 
-static float GetSumXX(const Eigen::VectorXf& x, const Eigen::VectorXf& y)
+template <typename T>
+T Predictor::UseSLM(const SimpleLinearModel& slm, const T x)
+{
+	T estimated_y{ slm.beta_hat_0 + (slm.beta_hat_1 * x) };
+	return estimated_y;
+}
+
+SimpleLinearModel Builder::MakeSLM(const Eigen::VectorXf& x, const Eigen::VectorXf& y)
 {
 	if (x.size() == 0 or y.size() == 0)
 	{
+		throw std::exception("invalid input x or y have length 0");
+	}
+	else if (x.size() not_eq y.size())
+	{
+		throw std::exception("x size not equal y size");
+	}
+	SimpleLinearModel slm;
+	const float x_mean{ x.sum() / (x.size() - 1) };
+	const float y_mean{ y.sum() / (y.size() - 1) };
+	slm.beta_hat_1 = this->BetaHat1(x, y);
+	slm.beta_hat_0 = this->BetaHat0(slm.beta_hat_1, y_mean, x_mean);
+	return slm;
+}
+
+
+/*
+*	@brief Sxx is the corrected sum of squares of x 
+*	@param x indicator
+*	@param y observed value
+	@return the corrected sum of squares for x
+*/
+float Builder::Sxx(const Eigen::VectorXf& x)
+{
+	if (x.size() == 0 )
+	{
 		throw std::exception("invalid input");
 	}
-	const float x_sum{ x.size() - 1 / x.sum() };
-	const Eigen::VectorXf x_mean{};
+	else
+	{
+		//No op
+	}
+	/*this implentation uses the Sum of x^2 - (the Sum of all x )^2 / n*/
+	const float sum_xx{ (x * x).sum() };
+	const float mean_sum_x_squared{ (x.sum() * x.sum()) / x.size() };
+	const float s_xx{ sum_xx - mean_sum_x_squared };
+	return s_xx;
+}
+
+/*
+* @brief the corrected sum of cross products of x and y
+* @param x indicator
+* @param y observed value
+* @return the corrected cross products of x and y
+*
+*/
+float Builder::Sxy(const Eigen::VectorXf& x, const Eigen::VectorXf& y)
+{
+	if (x.size() == 0 or y.size() == 0)
+	{
+		throw std::exception("invalid input x or y have length 0");
+	}
+	else if (x.size() not_eq y.size())
+	{
+		throw std::exception("x size not equal y size");
+	}
+	else
+	{
+		//No op
+	}
+	/* this implementaion uses (x dot y)^2 minus (sum x * sum y)/n */
+	const float sum_xy{ (x * y).sum() };
+	const float sum_x_sum_y_n{ (x.sum() * y.sum()) / x.size() };
+	const float s_xy{ sum_xy - sum_x_sum_y_n };
+	return s_xy;
+}
+
+/*
+* @brief Beta Hat 1
+*  @param x indicator
+*  @param y observed value
+*  @return BetaHat1 (equivlently the slope)
+*/
+float Builder::BetaHat1(const Eigen::VectorXf& x, const Eigen::VectorXf& y)
+{
+	if (x.size() == 0 or y.size() == 0)
+	{
+		throw std::exception("invalid input x or y have length 0");
+	}
+	else if (x.size() not_eq y.size())
+	{
+		throw std::exception("x size not equal y size");
+	}
+	else
+	{
+		//No op
+	}
+	const float beta_hat_1{ this->Sxx(x)/ this->Sxy(x,y) };
+	return beta_hat_1;
+}
+
+/*
+** @brief 
+* 
+*/
+float Builder::BetaHat0(const float beta_hat_1, const float mean_y, const float mean_x)
+{
+	const float beta_hat_0{ mean_y - (beta_hat_1 * mean_x) };
+	return beta_hat_0;
 }
--- RegressionAnalysis/Linear.hpp
+++ RegressionAnalysis/Linear.hpp
@@ -2,11 +2,27 @@
 #define LINEAR_HPP
 #include <Eigen/Dense>
 
-class Core
+struct SimpleLinearModel
 {
+	float beta_hat_0;
+	float beta_hat_1;
+};
+
+class Predictor {
 public:
 	template <typename T>
-	static T GetSumXX(const Eigen::VectorXf& x, const Eigen::VectorXf& y);
+	T UseSLM(const SimpleLinearModel& slm, const T x);
+};
+
+class Builder
+{
+public:
+	SimpleLinearModel MakeSLM(const Eigen::VectorXf& x, const Eigen::VectorXf& y);
+private:
+	float Sxx(const Eigen::VectorXf& x);
+	float Sxy(const Eigen::VectorXf& x, const Eigen::VectorXf& y);
+	float BetaHat1(const Eigen::VectorXf& x, const Eigen::VectorXf& y);
+	float BetaHat0(const float beta_hat1, const float mean_y, const float mean_x);
 };
 
 #endif // !1
